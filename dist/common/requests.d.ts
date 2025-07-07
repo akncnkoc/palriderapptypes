c4b1d6d2c1c943cdeef68a867d6ea17b7396f1c8
akncnkoc <akincankoc@gmail.com>
2025-07-07 15:38:02 +0300

    update

--- dist/common/requests.d.ts
+++ dist/common/requests.d.ts
@@ -1,3 +1,4 @@
+import { BaseUserAppState } from ".";
 export type PaginationRequestDTO = {
     page: number;
     size: number;
@@ -9,4 +10,19 @@ export type SearchAndSortRequestDTO = {
 };
 export type UpdateNotificationInfoRequestDTO = {
     one_signal_app_player_id: string;
+};
+export type UpdateUserAppInformationRequestDTO = {
+    app_version_id: string;
+    app_state: BaseUserAppState;
+    platform: string;
+    device_model: string;
+    os_version: string;
+    device_info_json: string;
+    last_seen_at: string;
+    app_opened_at: string;
+    app_closed_at: string;
+    network_type: string;
+    carrier: string;
+    language: string;
+    timezone: string;
 };
--- src/common/requests.ts
+++ src/common/requests.ts
@@ -1,3 +1,5 @@
+import { BaseUserAppState } from ".";
+
 export type PaginationRequestDTO = {
   page: number;
   size: number;
@@ -11,4 +13,20 @@ export type SearchAndSortRequestDTO = {
 
 export type UpdateNotificationInfoRequestDTO = {
   one_signal_app_player_id: string;
+};
+
+export type UpdateUserAppInformationRequestDTO = {
+  app_version_id: string;
+  app_state: BaseUserAppState;
+  platform: string;
+  device_model: string;
+  os_version: string;
+  device_info_json: string;
+  last_seen_at: string;
+  app_opened_at: string;
+  app_closed_at: string;
+  network_type: string;
+  carrier: string;
+  language: string;
+  timezone: string;
 };
