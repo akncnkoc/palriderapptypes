0c9a6d524f7b88719303654f1afadb90153ad36b
akncnkoc <akncnkoc@gmail.com>
2025-08-17 22:39:36 +0300

    update

--- src/company/index.ts
+++ src/company/index.ts
@@ -123,6 +123,7 @@ export type CompanyAreaAddressWalletDTO = {
   id: string;
   name: string;
   company_area_id: string;
+  company_area_address_id: string;
   balance: number;
   inconsumable_balance: number;
   created_at: string;
